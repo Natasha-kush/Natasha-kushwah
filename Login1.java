bd0b42834639713cc09c877646bf3beacd4cc665
Natasha-kush <84273323+Natasha-kush@users.noreply.github.com>
2022-09-24 23:20:04 +0530

    Add files via upload

---
+++ Login1.java
@@ -0,0 +1,128 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+ 
+class Login1 extends JFrame implements ActionListener {
+ 
+    
+    private Container c;
+    private JLabel title;
+    private JLabel name;
+    private JTextField tname;
+    private JLabel mno;
+    private JTextField tmno;
+    private JLabel gender;
+    private JRadioButton male;
+    private JRadioButton female;
+    private JLabel add;
+    private ButtonGroup gengp;
+    private JTextArea tadd;
+    
+    private JButton sub;
+    private JLabel res;
+    
+    public Login1()
+    {
+        setTitle("Registration Form");
+        setBounds(300, 90, 900, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+ 
+        c = getContentPane();
+        c.setLayout(null);
+ 
+        title = new JLabel("Registration Form");
+        title.setFont(new Font("Arial", Font.PLAIN, 30));
+        title.setSize(300, 30);
+        title.setLocation(300, 30);
+        c.add(title);
+ 
+        name = new JLabel("Name");
+        name.setFont(new Font("Arial", Font.PLAIN, 20));
+        name.setSize(100, 20);
+        name.setLocation(100, 100);
+        c.add(name);
+ 
+        tname = new JTextField();
+        tname.setFont(new Font("Arial", Font.PLAIN, 15));
+        tname.setSize(190, 20);
+        tname.setLocation(200, 100);
+        c.add(tname);
+ 
+        mno = new JLabel("Mobile");
+        mno.setFont(new Font("Arial", Font.PLAIN, 20));
+        mno.setSize(100, 20);
+        mno.setLocation(100, 150);
+        c.add(mno);
+ 
+        tmno = new JTextField();
+        tmno.setFont(new Font("Arial", Font.PLAIN, 15));
+        tmno.setSize(150, 20);
+        tmno.setLocation(200, 150);
+        c.add(tmno);
+ 
+        gender = new JLabel("Gender");
+        gender.setFont(new Font("Arial", Font.PLAIN, 20));
+        gender.setSize(100, 20);
+        gender.setLocation(100, 200);
+        c.add(gender);
+ 
+        male = new JRadioButton("Male");
+        male.setFont(new Font("Arial", Font.PLAIN, 15));
+        male.setSelected(true);
+        male.setSize(75, 20);
+        male.setLocation(200, 200);
+        c.add(male);
+ 
+        female = new JRadioButton("Female");
+        female.setFont(new Font("Arial", Font.PLAIN, 15));
+        female.setSelected(false);
+        female.setSize(80, 20);
+        female.setLocation(275, 200);
+        c.add(female);
+ 
+        gengp = new ButtonGroup();
+        gengp.add(male);
+        gengp.add(female);
+ 
+        
+        add = new JLabel("Address");
+        add.setFont(new Font("Arial", Font.PLAIN, 20));
+        add.setSize(100, 20);
+        add.setLocation(100, 300);
+        c.add(add);
+ 
+        tadd = new JTextArea();
+        tadd.setFont(new Font("Arial", Font.PLAIN, 15));
+        tadd.setSize(200, 75);
+        tadd.setLocation(200, 300);
+        tadd.setLineWrap(true);
+        c.add(tadd);
+ 
+       
+ 
+        sub = new JButton("Submit");
+        sub.setFont(new Font("Arial", Font.PLAIN, 15));
+        sub.setSize(100, 20);
+        sub.setLocation(150, 450);
+        sub.addActionListener(this);
+        c.add(sub);
+
+        res=new JLabel("");
+        res.setBounds(150,500,150,20);
+        c.add(res);
+ 
+        setVisible(true);
+    }
+ 
+   
+    public void actionPerformed(ActionEvent e)
+    {
+       res.setText("registration sucessfull");
+}
+ 
+     public static void main(String[] args) 
+    {
+      new Login1();
+    }
+}
